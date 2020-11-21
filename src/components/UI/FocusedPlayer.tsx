e158924018e89669f56aa14c2599d8e4065fcef7
bryjch <bryanch7@gmail.com>
2020-11-22 03:43:17 +0800

    add ui indicating player is respawning

--- src/components/UI/FocusedPlayer.tsx
+++ src/components/UI/FocusedPlayer.tsx
@@ -29,56 +29,95 @@ export const FocusedPlayer = (props: FocusedPlayerProps) => {
   icon = <ClassIcon classId={focused.classId} />
 
   return (
-    <>
-      <div className="panel">
+    <div className="container">
+      <div className="state">{health === 0 && <div className="dead">*RESPAWNING*</div>}</div>
+
+      <div className="player">
         <div className={`health ${healthCls}`}>{health}</div>
-        <div className="name">{name}</div>
-        <div className="class-icon-container">{icon}</div>
+
+        <div className="panel">
+          <div className="name">{name}</div>
+          <div className="class-icon-container">{icon}</div>
+        </div>
       </div>
 
       <style jsx>{`
-        .panel {
+        .container {
           display: flex;
-          flex-flow: row nowrap;
+          flex-flow: column nowrap;
           align-items: center;
-          max-width: 260px;
-          background-color: rgba(0, 0, 0, 0.4);
-          color: rgba(255, 255, 255, 1);
-          font-size: 1rem;
-          padding: 0.1rem 0;
-          border-bottom-width: 6px;
-          border-bottom-style: solid;
-          border-bottom-color: ${ACTOR_TEAM_COLORS(focused.team).focusedBackground};
 
-          .health {
-            position: relative;
-            color: #ffffff;
-            font-size: 1.5rem;
-            line-height: 1.5rem;
-            font-weight: 700;
-            padding: 0 0.3rem;
+          .state {
+            margin-bottom: 1rem;
 
-            &.overhealed {
-              color: ${ACTOR_TEAM_COLORS(focused.team).healthOverhealed};
+            .dead {
+              font-size: 2rem;
+              font-weight: 900;
+              color: #fbff09;
+              text-shadow: 0 0 3px #0a0a0a;
+              animation: dead-pulse 0.3s ease-in infinite alternate;
             }
 
-            &.low {
-              color: ${ACTOR_TEAM_COLORS(focused.team).healthLow};
+            @keyframes dead-pulse {
+              0% {
+                opacity: 0.5;
+              }
+              100% {
+                opacity: 1;
+              }
             }
           }
 
-          .name {
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            padding: 0 0.3rem;
-          }
+          .player {
+            display: flex;
+            flex-flow: row nowrap;
 
-          .class-icon-container {
-            padding: 0 0.3rem;
+            .health {
+              position: relative;
+              font-weight: 900;
+              color: #fff;
+              font-size: 2.5rem;
+              text-shadow: 0 0 3px #000000;
+              line-height: 2.5rem;
+              width: 65px;
+              text-align: center;
+
+              &.overhealed {
+                color: ${ACTOR_TEAM_COLORS(focused.team).healthOverhealed};
+              }
+
+              &.low {
+                color: ${ACTOR_TEAM_COLORS(focused.team).healthLow};
+              }
+            }
+
+            .panel {
+              display: flex;
+              flex-flow: row nowrap;
+              align-items: center;
+              max-width: 260px;
+              background-color: rgba(0, 0, 0, 0.3);
+              color: rgba(255, 255, 255, 1);
+              font-size: 1rem;
+              padding: 0.1rem 0;
+              border-bottom-width: 6px;
+              border-bottom-style: solid;
+              border-bottom-color: ${ACTOR_TEAM_COLORS(focused.team).focusedBackground};
+
+              .name {
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                padding: 0 0.3rem;
+              }
+
+              .class-icon-container {
+                padding: 0 0.3rem;
+              }
+            }
           }
         }
       `}</style>
-    </>
+    </div>
   )
 }
