ab853e0c1e0f3ba872374cf8b524f95d18679ff7
dylansq <dylansquinn@gmail.com>
2023-11-23 10:12:34 -0800

    Update Skybox.tsx

--- src/components/Scene/Skybox.tsx
+++ src/components/Scene/Skybox.tsx
@@ -13,10 +13,10 @@ export const Skybox2D = (props: WorldProps) => {
   //const [mapSkybox2D, setMapSkybox2D] = useState<THREE.Group | null>()
   const { map } = props
   const { scene } = useThree()
-  
+
   useEffect(() => {
-    
-    
+
+
     try {
       const mapSkyboxFileUrls = getMapSkyboxUrls(map)
 
@@ -24,19 +24,19 @@ export const Skybox2D = (props: WorldProps) => {
         alert('Unable to load map skybox.')
         return
       }
-        const skyboxURLS = [
-            mapSkyboxFileUrls.lf,
-            mapSkyboxFileUrls.rt,
-            mapSkyboxFileUrls.bk,
-            mapSkyboxFileUrls.ft,
-            mapSkyboxFileUrls.up,
-            mapSkyboxFileUrls.dn
-        ]
-        const skyboxLoader = new THREE.CubeTextureLoader()
-        const skyboxTexture = skyboxLoader.load(skyboxURLS)
-        scene.background = skyboxTexture
-        
-      
+      const skyboxURLS = [
+        mapSkyboxFileUrls.lf,
+        mapSkyboxFileUrls.rt,
+        mapSkyboxFileUrls.bk,
+        mapSkyboxFileUrls.ft,
+        mapSkyboxFileUrls.up,
+        mapSkyboxFileUrls.dn
+      ]
+      const skyboxLoader = new THREE.CubeTextureLoader()
+      const skyboxTexture = skyboxLoader.load(skyboxURLS)
+      scene.background = skyboxTexture
+
+
     } catch (error) {
       alert(
         `Unable to load skybox for map: ${map} \nThe project is probably missing the necessary files.`
